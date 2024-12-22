8796eb32f8e8782018529e570249ca49f2663c9b
CarlF3 <156613411+CarlF3@users.noreply.github.com>
2024-12-22 18:33:49 +0000

    Implemented shooting

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 from constants import *
 
 def main():
@@ -16,6 +17,7 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     
     Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
@@ -23,6 +25,8 @@ def main():
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
     AsteroidField()
+
+    Shot.containers = (shots, updatable, drawable)
 
 
     clock = pygame.time.Clock()
