6f64d6cfa35937779e39fd9c07682a7ebd0cf751
CarlF3 <156613411+CarlF3@users.noreply.github.com>
2024-12-22 16:56:30 +0000

    Removed unnecessary lines

--- asteroid.py
+++ asteroid.py
@@ -2,8 +2,6 @@ import pygame
 from circleshape import CircleShape
 
 class Asteroid(CircleShape):
-    containers = ()
-
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
 
